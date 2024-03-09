21962c6a77898d91c38474ee142505358854b306
Finley <18515185303@163.com>
2024-03-09 21:56:00 +0800

    dataset

--- Qwen-qlora-demo/component/dataset.py
+++ Qwen-qlora-demo/component/dataset.py
@@ -25,3 +25,36 @@ class SFTDataset(Dataset):
         return len(self.data_list)
 
     def __getitem__(self, index):
+        # 数据格式：<s>input1</s>target1</s>input2</s>target2</s>...
+        data = self.data_list[index]
+        data = json.loads(data)
+        conversation = data["conversation"]
+
+        # 收集多轮对话
+        utterances = []
+        for x in conversation:
+            utterances.append(x["human"])
+            utterances.append(x["assistant"])
+        utterances_ids = self.tokenizer(utterances, add_special_tokens=False).input_ids
+        input_ids = [self.bos_token_id]
+        target_mask = [0]
+        for i, utterances_id in enumerate(utterances_ids):
+            input_ids += (utterances_id + [self.eos_token_id])
+            # 用于对input进行mask，只计算target部分的loss
+            if i % 2 == 0:
+                target_mask += [0] * (len(utterances_id) + 1)
+            else:
+                target_mask += [1] * (len(utterances_id) + 1)
+        assert len(input_ids) == len(target_mask)
+        input_ids = input_ids[:self.max_seq_length]
+        target_mask = target_mask[:self.max_seq_length]
+        attention_mask = [1] * len(input_ids)
+        assert len(input_ids) == len(target_mask) == len(attention_mask)
+        inputs = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "target_mask": target_mask
+        }
+        return inputs
+
+
